a213c2ae517b63e601f2947dc168402ec53a429c
M T <mt@Ms-MacBook-Pro.local>
2021-08-27 09:26:05 +0300

    main - updated readme

---
+++ app/Http/Controllers/CarriageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Carriage;
+use Illuminate\Http\Request;
+
+class CarriageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Carriage  $carriage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Carriage $carriage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Carriage  $carriage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Carriage $carriage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Carriage  $carriage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Carriage $carriage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Carriage  $carriage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Carriage $carriage)
+    {
+        //
+    }
+}
